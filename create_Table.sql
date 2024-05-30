d50ed7092c466b01cdefa6e3f3f54c7ccbbb2364
prathapRaju5544 <raghavaraju.prathapraju@outlook.com>
2024-05-30 12:32:58 +0530

    creating table in database

---
+++ create_Table.sql
@@ -0,0 +1,9 @@
+create Table database : 
+syntex : create table "table_name";
+create table Departments(
+dno int primary key  AUTO_INCREMENT ,
+departmentName varchar(50)not null,
+salary  varchar(50)not null
+);
+
+select * from Departments;
